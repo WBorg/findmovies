d06b104d7c21c4a8a60542976e6e923be10875f5
Wborg <wilicious@gmail.com>
2023-03-02 12:34:32 -0300

    update 02/03/22

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,33 @@
-import {Center, Text} from 'native-base'
+import { Center, Text, Button } from 'native-base'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import api from '../services/api'
 
-export function Home(){
+export function Home() {
+  const [movies, setMovies] = useState([])
 
+  async function handleFindMovies() {
+    const response = await api.get('peça')
+    const data = await response.data
 
-  return(
+    setMovies(data.results)
+  }
+
+  return (
     <Center flex={1} backgroundColor="gray.700">
-      <Text>Find Movies</Text>
+      <Text color="white" fontSize="2xl">
+        Find Movies
+      </Text>
+      <Button marginTop="41px" onPress={handleFindMovies}>
+        <Text>Pesquisar filmes</Text>
+      </Button>
+
+      <FlatList
+        data={movies}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Text color="white">{item.title}</Text>}
+      />
     </Center>
   )
 }
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export default axios.create({
-  baseURL: ''
+  baseURL:
+    'https://api.themoviedb.org/3/search/movie?api_key=f6e7f056f145d0106056f930ab16e884&language=pt-BR&region=BR&query='
 })
