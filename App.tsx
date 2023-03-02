ceec0807e7e1afee151b4926fc7131a24725cf5f
Wborg <wilicious@gmail.com>
2023-03-02 02:14:56 -0300

    fail

---
+++ App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {NativeBaseProvider} from 'native-base'
+import { Home } from './src/pages/Home';
+
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Home/>
+    </NativeBaseProvider>
+  );
+}
+
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import {Center, Text} from 'native-base'
+
+export function Home(){
+
+
+  return(
+    <Center flex={1} backgroundColor="gray.700">
+      <Text>Find Movies</Text>
+    </Center>
+  )
+}
